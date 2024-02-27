2e1a776825ea0daa14e78dea44045942e115ef54
ethan <ethan.yang@intel.com>
2024-02-27 07:50:10 -0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # chatglm3.openvino
+
+**1. Set-up the environments:**
+
+```
+python3 -m venv openvino_env
+
+source openvino_env/bin/activate
+
+python3 -m pip install --upgrade pip
+
+pip install wheel setuptools
+
+pip install -r requirements.txt
+```
+
+**2. Convert model:**
+
+```
+python3 convert.py
+```
+
+**3. Quantize model:**
+
+```
+python3 quantize.py
+```
+
+**4. Run generation:**
+
+```
+python3 generate.py -m "./chatglm3_compressed"
+```
---
+++ generate.py
@@ -0,0 +1,154 @@
+import argparse
+from optimum.intel.openvino import OVModelForCausalLM
+from transformers import PretrainedConfig, AutoTokenizer, AutoConfig
+
+from optimum.utils import NormalizedTextConfig, NormalizedConfigManager
+from optimum.intel.openvino import OVModelForCausalLM
+from optimum.intel.openvino.utils import OV_XML_FILE_NAME
+from pathlib import Path
+from typing import Optional, Union, Dict
+from pathlib import Path
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-h',
+                        '--help',
+                        action='help',
+                        help='Show this help message and exit.')
+    parser.add_argument('-m',
+                        '--model_path',
+                        required=True,
+                        type=str,
+                        help='Required. model path')
+    parser.add_argument('-p',
+                        '--prompt',
+                        default="请介绍一下上海？",
+                        required=False,
+                        type=str,
+                        help='Required. prompt sentence')
+    parser.add_argument('-l',
+                        '--max_sequence_length',
+                        default=128,
+                        required=False,
+                        type=int,
+                        help='Required. maximun lengh of output')
+    parser.add_argument('-d',
+                        '--device',
+                        default='CPU',
+                        required=False,
+                        type=str,
+                        help='Required. device for inference')
+    args = parser.parse_args()
+
+
+class OVCHATGLMModel(OVModelForCausalLM):
+    """
+    Optimum intel compatible model wrapper for CHATGLM2
+    """
+
+    def __init__(
+        self,
+        model: "Model",
+        config: "PretrainedConfig" = None,
+        device: str = "CPU",
+        dynamic_shapes: bool = True,
+        ov_config: Optional[Dict[str, str]] = None,
+        model_save_dir: Optional[Union[str, Path]] = None,
+        **kwargs,
+    ):
+        NormalizedConfigManager._conf["chatglm"] = NormalizedTextConfig.with_args(
+            num_layers="num_hidden_layers",
+            num_attention_heads="num_attention_heads",
+            hidden_size="hidden_size",
+        )
+        super().__init__(
+            model, config, device, dynamic_shapes, ov_config, model_save_dir, **kwargs
+        )
+
+    def _reshape(
+        self,
+        model: "Model",
+        batch_size: int,
+        sequence_length: int,
+        height: int = None,
+        width: int = None,
+    ):
+        shapes = {}
+        for inputs in model.inputs:
+            shapes[inputs] = inputs.get_partial_shape()
+            shapes[inputs][0] = -1
+            input_name = inputs.get_any_name()
+            if input_name.startswith('beam_idx'):
+                continue
+            if input_name.startswith('past_key_values'):
+                shapes[inputs][1] = -1
+                shapes[inputs][2] = 2
+            elif shapes[inputs].rank.get_length() > 1:
+                shapes[inputs][1] = -1
+        model.reshape(shapes)
+        return model
+
+    @classmethod
+    def _from_pretrained(
+        cls,
+        model_id: Union[str, Path],
+        config: PretrainedConfig,
+        use_auth_token: Optional[Union[bool, str, None]] = None,
+        revision: Optional[Union[str, None]] = None,
+        force_download: bool = False,
+        cache_dir: Optional[str] = None,
+        file_name: Optional[str] = None,
+        subfolder: str = "",
+        from_onnx: bool = False,
+        local_files_only: bool = False,
+        load_in_8bit: bool = False,
+        **kwargs,
+    ):
+        model_path = Path(model_id)
+        default_file_name = OV_XML_FILE_NAME
+        file_name = file_name or default_file_name
+
+        model_cache_path = cls._cached_file(
+            model_path=model_path,
+            use_auth_token=use_auth_token,
+            revision=revision,
+            force_download=force_download,
+            cache_dir=cache_dir,
+            file_name=file_name,
+            subfolder=subfolder,
+            local_files_only=local_files_only,
+        )
+
+        model = cls.load_model(model_cache_path)
+        init_cls = OVCHATGLMModel
+
+        return init_cls(
+            model=model, config=config, model_save_dir=model_cache_path.parent, **kwargs
+        )
+
+
+ov_config = {"PERFORMANCE_HINT": "LATENCY",
+             "NUM_STREAMS": "1", "CACHE_DIR": ""}
+
+
+tok = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
+model_dir = args.model_path
+
+ov_model = OVCHATGLMModel.from_pretrained(
+    model_dir,
+    device=args.device,
+    ov_config=ov_config,
+    config=AutoConfig.from_pretrained(model_dir, trust_remote_code=True),
+    trust_remote_code=True,
+)
+
+tokenizer_kwargs = {"add_special_tokens": False}
+input_ids = []
+input_ids.extend(tok.build_single_message("user", "", args.prompt))
+input_ids.extend([tok.get_command("<|assistant|>")])
+input_token = tok.batch_encode_plus(
+    [input_ids], return_tensors="pt", is_split_into_words=True
+).input_ids
+answer = ov_model.generate(
+    input_token, max_new_tokens=args.max_sequence_length)
+print(tok.batch_decode(answer, skip_special_tokens=True)[0])
